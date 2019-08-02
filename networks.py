7508ab06f3f2f2ac9a8ddd53eab2f9d47856ebb2
hkx <hkx444@live.com>
2019-08-02 14:49:23 +0800

    [feature] a Rhoclipper for constraint the "rho" value in AdaILN and ILN like TF version and Paper

--- networks.py
+++ networks.py
@@ -249,3 +249,16 @@ class Discriminator(nn.Module):
         return out, cam_logit, heatmap
 
 
+class RhoClipper(object):
+
+    def __init__(self, min, max):
+        self.clip_min = min
+        self.clip_max = max
+        assert min < max
+
+    def __call__(self, module):
+
+        if hasattr(module, 'rho'):
+            w = module.rho.data
+            w = w.clamp(self.clip_min, self.clip_max)
+            module.rho.data = w
